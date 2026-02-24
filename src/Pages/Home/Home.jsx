5c17ce7d10cad2b7eac9c53424ce9169c6cb6e64
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-02-24 13:49:52 +0600

    lawyers section started

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../component/Banner/Banner";
+import Lawyer from "../../component/Lawyer/Lawyer";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Lawyer></Lawyer>
     </div>
   );
 };
--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -40,7 +40,7 @@ const Banner = () => {
           opacity: 0.9,
         }}
       >
-        Our platform connects you with verified, experienced doctors across
+        Our platform connects you with verified, experienced Lawyers across
         various specialties — all at your convenience. Whether it's a routine
         checkup or urgent consultation, book appointments in minutes and receive
         quality care you can trust.
