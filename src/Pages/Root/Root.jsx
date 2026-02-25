2df35442df71a1a69fb9230a5c6a6879fa5caf97
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-02-25 18:53:50 +0600

    dynamic title change implemented

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../../component/Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../../component/Footer/Footer";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useLocation } from "react-router";
 
 const Root = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+
+    if (path === "/") {
+      document.title = "DocTalk";
+    } else if (path === "/booking") {
+      document.title = "Booking";
+    } else if (path === "/contact_us") {
+      document.title = "Contact Us";
+    } else if (path === "/emergency") {
+      document.title = "Emergency";
+    } else if (path === "/blog") {
+      document.title = "Blog";
+    } else if (path.startsWith("/doctordetails/")) {
+      document.title = "Doctor Details";
+    }
+  }, [location]);
   return (
     <>
       <Header></Header>
